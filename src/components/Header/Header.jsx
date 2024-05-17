6a93c65fc8d7eff14fdd61e1a855e433fd2fff74
adervish <adervishaj159@gmail.com>
2024-05-16 20:04:48 -0400

    starting the header

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,19 @@
+import "./Header.css";
+import avatar from "../../assets/avatar.svg";
+import logo from "../../assets/logo.svg";
+
+function Header() {
+  return (
+    <Header className="header">
+      <img className="header__logo" alt="header-logo" src={logo} />
+      <p className="header__date-and-location">DATE, LOCATION</p>
+      <button className="header__add-clothes-button">+ Add clothes</button>
+      <div className="header__user-container">
+        <p className="header__username">Terrence Tegegne</p>
+        <img src={avatar} alt="Terrence Tegegne" className="header__avatar" />
+      </div>
+    </Header>
+  );
+}
+
+export default Header;
